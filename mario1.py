ff901c3a0eb4d7bce8916fdc4f26ddd08229170c
David J. Malan <malan@harvard.edu>
2018-04-10 15:06:19 -0400

    first commit

---
+++ mario1.py
@@ -0,0 +1,9 @@
+while True:
+    n = int(input("Size: "))
+    if n >= 1 and n <= 8:
+        break
+
+for i in range(n):
+    for j in range(i + 1):
+        print("#", end="")
+    print()
---
+++ mario2.py
@@ -0,0 +1,11 @@
+while True:
+    n = int(input("Size: "))
+    if n >= 1 and n <= 8:
+        break
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print(".", end="")
+    for j in range(i + 1):
+        print("#", end="")
+    print()
